f5d9575a4a09b25f5fe3ba5cd5a4d8d1e3ce5ec6
Thomas Breydo <tbreydo@gmail.com>
2021-05-30 14:24:28 -0400

    Create Quadratic with a, b, c and discriminant

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Quadratic {
+/** Second-degree polynomial in the form ax^2 + bx + c. */
+public class Quadratic {
+  private final double a, b, c, discriminant;
+
+  /**
+   * @param a quadratic coefficient
+   * @param b linear coefficient
+   * @param c constant coefficient
+   */
+  Quadratic(double a, double b, double c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    discriminant = b * b - 4 * a * c;
+  }
+
+  public double getA() {
+    return a;
+  }
+
+  public double getB() {
+    return b;
+  }
+
+  public double getC() {
+    return c;
+  }
+
+  /**
+   * Checks if this quadratic has real roots.
+   *
+   * @return {@code true} if this {@code Quadratic} has real roots
+   */
+  public boolean hasRealRoots() {
+    return discriminant >= 0;
+  }
+
+  /**
+   * Returns the number of roots this quadratic has.
+   *
+   * @return the number of roots this quadratic has
+   */
+  public int numberOfRoots() {
+    if (discriminant > 0) return 2;
+    if (discriminant == 0) return 1;
+    return 0;
+  }
 }
--- test/QuadraticTest.java
+++ test/QuadraticTest.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class QuadraticTest {
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class QuadraticTest {
+  @Test
+  public void getA() {
+    Quadratic q = new Quadratic(1, 2, 3);
+
+    assertThat(q.getA(), is(1.0));
+  }
+
+  @Test
+  public void getB() {
+    Quadratic q = new Quadratic(1, 2, 3);
+
+    assertThat(q.getB(), is(2.0));
+  }
+
+  @Test
+  public void getC() {
+    Quadratic q = new Quadratic(1, 2, 3);
+
+    assertThat(q.getC(), is(3.0));
+  }
+
+  @Test
+  public void hasRealRoots() {
+    Quadratic q1 = new Quadratic(1, 2, 3);
+    Quadratic q2 = new Quadratic(1, 0, -4);
+    assertThat(q1.hasRealRoots(), is(false));
+    assertThat(q2.hasRealRoots(), is(true));
+  }
+
+  @Test
+  public void numberOfRoots() {
+    Quadratic q0 = new Quadratic(4, 0, 1);
+    Quadratic q1 = new Quadratic(4, 0, 0);
+    Quadratic q2 = new Quadratic(4, 0, -1);
+    assertThat(q0.numberOfRoots(), is(0));
+    assertThat(q1.numberOfRoots(), is(1));
+    assertThat(q2.numberOfRoots(), is(2));
+  }
 }
