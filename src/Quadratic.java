399ccb8a26ce66d96798e4c3e1e421f6657d036b
Thomas Breydo <tbreydo@gmail.com>
2021-05-30 15:11:56 -0400

    Finish Quadratic project

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -1,6 +1,6 @@
 /** Second-degree polynomial in the form ax^2 + bx + c. */
 public class Quadratic {
-  private final double a, b, c, discriminant;
+  private final double a, b, c, discriminant, vertexX, vertexY;
 
   /**
    * @param a quadratic coefficient
@@ -12,6 +12,8 @@ public class Quadratic {
     this.b = b;
     this.c = c;
     discriminant = b * b - 4 * a * c;
+    vertexX = -b / (2 * a);
+    vertexY = evaluateWith(vertexX);
   }
 
   public double getA() {
@@ -36,13 +38,62 @@ public class Quadratic {
   }
 
   /**
-   * Returns the number of roots this quadratic has.
+   * Returns the number of roots that this quadratic has.
    *
-   * @return the number of roots this quadratic has
+   * @return the number of roots that this quadratic has
    */
   public int numberOfRoots() {
     if (discriminant > 0) return 2;
     if (discriminant == 0) return 1;
     return 0;
+  }
+  /**
+   * Returns the roots of this quadratic as an array of length 0, 1, or 2.
+   *
+   * @return the roots of this quadratic
+   */
+  public double[] getRootArray() {
+    if (discriminant < 0) return new double[] {};
+    if (discriminant == 0) return new double[] {vertexX};
+    double d = Math.sqrt(discriminant) / (2 * a);
+    return new double[] {vertexX - d, vertexX + d};
+  }
+
+  /**
+   * Returns the x-coordinate of this quadratic's vertex
+   *
+   * @return the x-coordinate of this quadratic's vertex
+   */
+  public double getAxisOfSymmetry() {
+    return vertexX;
+  }
+
+  /**
+   * Returns the y-coordinate of this quadratic's vertex
+   *
+   * @return the y-coordinate of this quadratic's vertex
+   */
+  public double getExtremeValue() {
+    return vertexY;
+  }
+
+  /**
+   * Checks whether the extreme value of this quadratic is an absolute maximum. If not, it's an
+   * absolute minimum.
+   *
+   * @return {@code true} if the extreme value of this quadratic is an absolute maximum.
+   */
+  public boolean isMax() {
+    return a < 0;
+  }
+
+  /**
+   * Evaluates this quadratic at {@code x}.
+   *
+   * @param x the input at which to evaluate
+   * @return ax^2 + bx + c
+   */
+  public double evaluateWith(double x) {
+    return x * (a * x + b) + c;
   }
 }
--- test/QuadraticTest.java
+++ test/QuadraticTest.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class QuadraticTest {
   @Test
@@ -29,6 +29,7 @@ public class QuadraticTest {
   public void hasRealRoots() {
     Quadratic q1 = new Quadratic(1, 2, 3);
     Quadratic q2 = new Quadratic(1, 0, -4);
+
     assertThat(q1.hasRealRoots(), is(false));
     assertThat(q2.hasRealRoots(), is(true));
   }
@@ -38,8 +39,50 @@ public class QuadraticTest {
     Quadratic q0 = new Quadratic(4, 0, 1);
     Quadratic q1 = new Quadratic(4, 0, 0);
     Quadratic q2 = new Quadratic(4, 0, -1);
+
     assertThat(q0.numberOfRoots(), is(0));
     assertThat(q1.numberOfRoots(), is(1));
     assertThat(q2.numberOfRoots(), is(2));
+  }
+
+  @Test
+  public void getRootArray() {
+    Quadratic q0 = new Quadratic(1, 0, 1);
+    Quadratic q1 = new Quadratic(1, -2, 1);
+    Quadratic q2 = new Quadratic(1, 0, -1);
+
+    assertThat(q0.getRootArray(), is(new double[] {}));
+    assertThat(q1.getRootArray(), is(new double[] {1}));
+    assertThat(q2.getRootArray(), is(new double[] {-1, 1}));
+  }
+
+  @Test
+  public void getAxisOfSymmetry() {
+    Quadratic q = new Quadratic(1, -1, 0);
+
+    assertThat(q.getAxisOfSymmetry(), is(0.5));
+  }
+
+  @Test
+  public void getExtremeValue() {
+    Quadratic q = new Quadratic(1, -1, 0);
+
+    assertThat(q.getExtremeValue(), is(-0.25));
+  }
+
+  @Test
+  public void isMax() {
+    Quadratic q1 = new Quadratic(1, -1, 0);
+    Quadratic q2 = new Quadratic(-1, 1, 0);
+
+    assertThat(q1.isMax(), is(false));
+    assertThat(q2.isMax(), is(true));
+  }
+
+  @Test
+  public void evaluateWith() {
+    Quadratic q = new Quadratic(1, 2, 3);
+
+    assertThat(q.evaluateWith(3), is(18.0));
   }
 }
