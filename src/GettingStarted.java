74cd0f9938e1220fa497a355f73e9a1f9d2de46e
Thomas Breydo <tbreydo@gmail.com>
2021-05-30 17:06:06 -0400

    Intro to OSP

--- src/GettingStarted.java
+++ src/GettingStarted.java
@@ -3,11 +3,13 @@ import org.dalton.polyfun.Polynomial;
 import org.opensourcephysics.display.Trail;
 import org.opensourcephysics.frames.PlotFrame;
 
+import javax.swing.*;
 import java.awt.Color;
 
 /** Introduction to Polynomials, Open Source Physics and JUnit test. */
 public class GettingStarted {
   public static void main(String[] args) {
+    // Intro to Polynomials
     Polynomial gx = new Polynomial(new double[] {5, 0, 0, 0, 4}); // const -> x^4
     Polynomial vx = new Polynomial(new double[] {1, 2, 3}); // const -> x^2
     Polynomial fx = new Polynomial(new double[] {0, 0, 1}); // const -> x^2
@@ -19,6 +21,27 @@ public class GettingStarted {
     int i = 0;
     for (double c : vx.getCoefficientArray()) {
       System.out.println(++i + ". " + c);
+    }
+
+    // Intro to OSP
+    PlotFrame plotFrame = new PlotFrame("x", "y", "f(x), v(x), g(x)");
+    plotFrame.setVisible(true); // need this to show the graph, it is false by default
+    plotFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    plotFrame.setPreferredMinMax(0, 10, 0, 15); // x and y ranges
+    plotFrame.setConnected(true);
+    Trail fTrail = new Trail();
+    Trail vTrail = new Trail();
+    Trail gTrail = new Trail();
+    fTrail.color = Color.RED;
+    vTrail.color = Color.GREEN;
+    gTrail.color = Color.YELLOW;
+    plotFrame.addDrawable(fTrail);
+    plotFrame.addDrawable(vTrail);
+    plotFrame.addDrawable(gTrail);
+    for (int x = 0; x <= 10; ++x) {
+      fTrail.addPoint(x, fx.eval(x));
+      vTrail.addPoint(x, vx.eval(x));
+      gTrail.addPoint(x, gx.eval(x));
     }
   }
 
