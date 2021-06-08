1119f2bbc17afcd5dea53e69fc3a3489c0067e28
Thomas Breydo <tbreydo@gmail.com>
2021-06-08 11:02:19 -0500

    Add Trapezoid plot

--- src/com/thomasbreydo/riemann/TrapezoidPlot.java
+++ src/com/thomasbreydo/riemann/TrapezoidPlot.java
@@ -1,2 +1,22 @@
-package com.thomasbreydo.riemann;public class TrapezoidPlot {
+package com.thomasbreydo.riemann;
+
+import org.opensourcephysics.frames.PlotFrame;
+
+public class TrapezoidPlot extends AbstractRiemann {
+  public TrapezoidPlot(
+      Function func, PlotFrame plotFrame, double xLower, double xUpper, int subintervals) {
+    super(func, plotFrame, xLower, xUpper, subintervals);
+  }
+
+  @Override
+  public double getSubintervalArea(double leftBorder, double rightBorder) {
+    return (rightBorder - leftBorder) * (func.eval(leftBorder) + func.eval(rightBorder)) / 2;
+  }
+
+  @Override
+  public void drawSlice(double leftBorder, double rightBorder) {
+    drawPath(
+        new double[] {leftBorder, leftBorder, rightBorder, rightBorder},
+        new double[] {0, func.eval(leftBorder), func.eval(rightBorder), 0});
+  }
 }
--- test/com/thomasbreydo/riemann/TrapezoidPlotTest.java
+++ test/com/thomasbreydo/riemann/TrapezoidPlotTest.java
@@ -4,15 +4,16 @@ import org.dalton.polyfun.Polynomial;
 import org.junit.Test;
 import org.opensourcephysics.frames.PlotFrame;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
-public class LeftHandPlotTest {
+public class TrapezoidPlotTest {
+  static final double DELTA = 1e-5; // tolerance for float checks
+
   @Test
   public void getSubintervalArea() {
-    Polynomial poly = new Polynomial(new double[] {0, 0, 1}); // x^2
+    Function func = new Function(new Polynomial(new double[] {0, 0, 1})); // x^2
     PlotFrame plotFrame = new PlotFrame("x", "y", "Left Hand Rule");
-    LeftHandPlot leftHandPlot = new LeftHandPlot(poly, plotFrame, 0, 2, 10);
-    assertThat(leftHandPlot.getIntervalArea(), is(3.08));
+    TrapezoidPlot trapezoidPlot = new TrapezoidPlot(func, plotFrame, 0, 2, 10);
+    assertEquals(trapezoidPlot.getIntervalArea(), 2.68, DELTA);
   }
 }
