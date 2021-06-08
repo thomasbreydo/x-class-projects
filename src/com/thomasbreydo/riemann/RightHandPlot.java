44b633ee69e95e7997cde5a57ce53806691bd2c9
Thomas Breydo <tbreydo@gmail.com>
2021-06-08 08:19:19 -0500

    Add Right-hand plot

--- src/com/thomasbreydo/riemann/RightHandPlot.java
+++ src/com/thomasbreydo/riemann/RightHandPlot.java
@@ -1,2 +1,24 @@
-package com.thomasbreydo.riemann;public class RightHandPlot {
+package com.thomasbreydo.riemann;
+
+import org.dalton.polyfun.Polynomial;
+import org.opensourcephysics.frames.PlotFrame;
+
+public class RightHandPlot extends AbstractRiemann {
+  public RightHandPlot(
+      Function func, PlotFrame plotFrame, double xLower, double xUpper, int subintervals) {
+    super(func, plotFrame, xLower, xUpper, subintervals);
+  }
+
+  @Override
+  public double getSubintervalArea(double leftBorder, double rightBorder) {
+    return (rightBorder - leftBorder) * func.eval(rightBorder);
+  }
+
+  @Override
+  public void drawSlice(double leftBorder, double rightBorder) {
+    double height = func.eval(rightBorder);
+    drawPath(
+        new double[] {leftBorder, leftBorder, rightBorder, rightBorder},
+        new double[] {0, height, height, 0});
+  }
 }
--- test/com/thomasbreydo/riemann/RightHandPlotTest.java
+++ test/com/thomasbreydo/riemann/RightHandPlotTest.java
@@ -1,3 +1,5 @@
+package com.thomasbreydo.riemann;
+
 import org.dalton.polyfun.Polynomial;
 import org.junit.Test;
 import org.opensourcephysics.frames.PlotFrame;
@@ -6,10 +8,14 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class RightHandPlotTest {
+
+  static final double DELTA = 1e-5; // tolerance for float checks
+
   @Test
   public void getSubintervalArea() {
-    Polynomial poly = new Polynomial(new double[] {0, 0, 1}); // x^2
-    RightHandPlot rightHandPlot =
-        new RightHandPlot(poly, new PlotFrame("x", "y", "Right Hand Rule"), 0, 2, 10);
+    Function func = new Function(new Polynomial(new double[] {0, 0, 1})); // x^2
+    PlotFrame plotFrame = new PlotFrame("x", "y", "Right Hand Rule");
+    RightHandPlot rightHandPlot = new RightHandPlot(func, plotFrame, 0, 2, 10);
+    assertEquals(rightHandPlot.getIntervalArea(), 3.08, DELTA);
   }
 }
