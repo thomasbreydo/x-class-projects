c9035f9639d669968d05219e36b03f246985fb9f
Thomas Breydo <tbreydo@gmail.com>
2021-09-17 18:34:30 -0400

    Finish first iteration of Projectile App

---
+++ src/com/thomasbreydo/projectile/Particle2D.java
@@ -0,0 +1,83 @@
+package com.thomasbreydo.projectile;
+
+import org.opensourcephysics.display.Circle;
+
+public class Particle2D extends Circle {
+  private static final double DRAG_CONSTANT = 0.02;
+  private static final double GRAVITY = -9.81;
+  private final double mass;
+  private final double weight;
+  private final double crossSectionalArea;
+  private final double[] acceleration;
+  private final double[] velocity;
+
+  public Particle2D(double radius, double mass, double initialX, double initialY) {
+    this(radius, mass, initialX, initialY, new double[] {0, 0}, new double[] {0, GRAVITY});
+  }
+
+  public Particle2D(
+      double radius, double mass, double initialX, double initialY, double[] initialVelocity) {
+    this(radius, mass, initialX, initialY, initialVelocity, new double[] {0, GRAVITY});
+  }
+
+  public Particle2D(
+      double radius,
+      double mass,
+      double initialX,
+      double initialHeight,
+      double[] initialVelocity,
+      double[] initialAcceleration) {
+    super(initialX, initialHeight);
+
+    if (initialVelocity.length != initialAcceleration.length)
+      throw new IllegalArgumentException(
+          "velocity and acceleration vectors have different number of dimensions");
+
+    this.mass = mass;
+    velocity = initialVelocity;
+    acceleration = initialAcceleration;
+
+    weight = mass * GRAVITY;
+    crossSectionalArea = 2 * Math.PI * radius;
+  }
+
+  void step(double deltaTime) {
+    if (y < 0) return;
+    updateHeight(deltaTime);
+    updateVelocity(deltaTime);
+  }
+
+  void step(double deltaTime, double pressure) {
+    if (y < 0) return;
+    updateHeight(deltaTime);
+    updateVelocity(deltaTime);
+    updateAcceleration(pressure);
+  }
+
+  void updateVelocity(double deltaTime) {
+    for (int i = 0; i < velocity.length; ++i) velocity[i] += deltaTime * acceleration[i];
+  }
+
+  void updateHeight(double deltaTime) {
+    x += deltaTime * velocity[0];
+    y += deltaTime * velocity[1];
+  }
+
+  void updateAcceleration(double pressure) {
+    double drag, totalNetForce;
+    for (int i = 0; i < velocity.length; ++i) {
+      drag =
+          pressure * DRAG_CONSTANT * crossSectionalArea * velocity[i] * velocity[i] / (mass * 2);
+      totalNetForce = weight + drag;
+      acceleration[i] = totalNetForce / mass;
+    }
+  }
+
+  public double[] getAcceleration() {
+    return acceleration;
+  }
+
+  public double[] getVelocity() {
+    return velocity;
+  }
+}
---
+++ src/com/thomasbreydo/projectile/ProjectileApp.java
@@ -0,0 +1,52 @@
+package com.thomasbreydo.projectile;
+
+import org.opensourcephysics.controls.AbstractSimulation;
+import org.opensourcephysics.controls.SimulationControl;
+import org.opensourcephysics.frames.PlotFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ProjectileApp extends AbstractSimulation {
+  static final int RADIUS = 10; // (m)
+  static final int MASS = 7; // (kg)
+  static final int INITIAL_X = 0; // (m)
+  static final int INITIAL_Y = 0; // (m)
+  static final double DELTA_TIME = 0.05; // (s)
+  static final double AIR_PRESSURE = 1.225; // at sea level, 15ºC (kg/m³)
+  static final double[] INITIAL_VELOCITY = {50, 10};
+  final PlotFrame plotFrame = new PlotFrame("x", "y", "Ball Simulation");
+  Particle2D particle1;
+  Particle2D particle2;
+  Particle2D[] particles;
+
+  public static void main(String[] args) {
+    SimulationControl.createApp(new ProjectileApp());
+  }
+
+  @Override
+  public void initialize() {
+    plotFrame.clearDrawables();
+    plotFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    plotFrame.setVisible(true);
+
+    particle1 = new Particle2D(RADIUS, MASS, INITIAL_X, INITIAL_Y, INITIAL_VELOCITY);
+    particle2 = new Particle2D(RADIUS, MASS, INITIAL_X, INITIAL_Y, INITIAL_VELOCITY);
+    particle1.color = Color.RED;
+    particle2.color = Color.GREEN;
+    particles = new Particle2D[] {particle1, particle2};
+
+    for (Particle2D particle : particles) plotFrame.addDrawable(particle);
+
+    plotFrame.setPreferredMinMax(0, 100, 0, 10);
+  }
+
+  @Override
+  public void reset() {}
+
+  public void doStep() {
+    if (particle1.getVelocity()[1] >= 0 || particle1.getY() > 0) particle1.step(DELTA_TIME);
+    if (particle2.getVelocity()[1] >= 0 || particle2.getY() > 0)
+      particle2.step(DELTA_TIME, AIR_PRESSURE);
+  }
+}
