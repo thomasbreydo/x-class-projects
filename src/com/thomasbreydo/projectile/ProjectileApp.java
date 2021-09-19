daed3cc71b9d065474e3e1bb23dc7a85d36a930d
Thomas Breydo <tbreydo@gmail.com>
2021-09-18 23:20:24 -0400

    Omit gravitational pull when computing dragX

--- src/com/thomasbreydo/projectile/ProjectileApp.java
+++ src/com/thomasbreydo/projectile/ProjectileApp.java
@@ -8,13 +8,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ProjectileApp extends AbstractSimulation {
-  static final int RADIUS = 10; // (m)
-  static final int MASS = 7; // (kg)
+  static final int RADIUS = 5; // (m)
+  static final int MASS = 10; // (kg)
   static final int INITIAL_X = 0; // (m)
   static final int INITIAL_Y = 0; // (m)
   static final double DELTA_TIME = 0.05; // (s)
   static final double AIR_PRESSURE = 1.225; // at sea level, 15ºC (kg/m³)
-  static final double[] INITIAL_VELOCITY = {50, 10};
+  static final double[] INITIAL_VELOCITY = {49, 9};
   final PlotFrame plotFrame = new PlotFrame("x", "y", "Ball Simulation");
   Particle2D particle1;
   Particle2D particle2;
@@ -30,8 +30,8 @@ public class ProjectileApp extends AbstractSimulation {
     plotFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     plotFrame.setVisible(true);
 
-    particle1 = new Particle2D(RADIUS, MASS, INITIAL_X, INITIAL_Y, INITIAL_VELOCITY.clone());
-    particle2 = new Particle2D(RADIUS, MASS, INITIAL_X, INITIAL_Y, INITIAL_VELOCITY.clone());
+    particle1 = new Particle2D(RADIUS, MASS, INITIAL_X, INITIAL_Y, INITIAL_VELOCITY);
+    particle2 = new Particle2D(RADIUS, MASS, INITIAL_X, INITIAL_Y, INITIAL_VELOCITY);
     particle1.color = Color.RED;
     particle2.color = Color.GREEN;
     particles = new Particle2D[] {particle1, particle2};
@@ -45,8 +45,8 @@ public class ProjectileApp extends AbstractSimulation {
   public void reset() {}
 
   public void doStep() {
-    if (particle1.getVelocity()[1] >= 0 || particle1.getY() > 0) particle1.step(DELTA_TIME);
-    if (particle2.getVelocity()[1] >= 0 || particle2.getY() > 0)
+    if (particle1.getVelocityY() >= 0 || particle1.getY() > 0) particle1.step(DELTA_TIME);
+    if (particle2.getVelocityY() >= 0 || particle2.getY() > 0)
       particle2.step(DELTA_TIME, AIR_PRESSURE);
   }
 }
