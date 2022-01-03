0d70ccdd534a27b04892fdb61e74a8b5cf8858d4
Thomas Breydo <tbreydo@gmail.com>
2022-01-03 18:46:21 -0500

    Add Matrix::plus(),switchRows() tests

--- test/com/thomasbreydo/matrix/MatrixTest.java
+++ test/com/thomasbreydo/matrix/MatrixTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertThrows;
 
 public class MatrixTest {
   static final double DELTA = 1e-5; // tolerance for float checks
-  Matrix matrix;
 
   public static void assertEquals(Matrix expected, Matrix actual) {
     Assert.assertEquals("row count differs", expected.getRowCount(), actual.getRowCount());
@@ -21,32 +20,74 @@ public class MatrixTest {
   @Test
   public void testRowReduce() {
     Matrix expected = new Matrix(new double[][] {{1, 0, 7.0 / 3.0}, {0, 1, -1.0 / 3.0}, {0, 0, 0}});
-    matrix = new Matrix(new double[][] {{0, 9, -3}, {1, 1, 2}, {3, 0, 7}});
+    Matrix m1 = new Matrix(new double[][] {{0, 9, -3}, {1, 1, 2}, {3, 0, 7}});
     Matrix same = new Matrix(new double[][] {{0, 9, -3}, {1, 1, 2}, {3, 0, 7}});
 
-    Matrix actual = matrix.rowReduce();
+    Matrix actual = m1.rowReduce();
     assertEquals(expected, actual);
-    assertEquals(same, matrix);
+    assertEquals(same, m1);
   }
 
   @Test
   public void testInvertInvertible() {
     Matrix expected = new Matrix(new double[][] {{1, 0, 0}, {0, 1.0 / 2.0, 0}, {0, 0, 1.0 / 3.0}});
-    matrix = new Matrix(new double[][] {{1, 0, 0}, {0, 2, 0}, {0, 0, 3}});
-    Matrix same = new Matrix(new double[][] {{1, 0, 0}, {0, 2, 0}, {0, 0, 3}});
+    Matrix m1 = new Matrix(new double[][] {{1, 0, 0}, {0, 2, 0}, {0, 0, 3}});
+    Matrix m1copy = m1.copy();
 
-    Matrix actual = matrix.invert();
+    Matrix actual = m1.invert();
     assertEquals(expected, actual);
-    assertEquals(same, matrix);
+    assertEquals(m1copy, m1);
   }
 
   @Test
   public void testInvertNonInvertible() {
-    matrix = new Matrix(new double[][] {{0, 0, 0}, {0, 2, 0}, {0, 0, 3}});
-    assertThrows(RuntimeException.class, matrix::invert);
-    matrix = new Matrix(new double[][] {{9, -1.001, 3}, {0, 0, 0}, {5, 0, 3}});
-    assertThrows(RuntimeException.class, matrix::invert);
-    matrix = new Matrix(new double[][] {{9, 0, 3}, {0, 0, 1.5333}, {2, 0, 1.0 / 7.0}});
-    assertThrows(RuntimeException.class, matrix::invert);
+    Matrix m1 = new Matrix(new double[][] {{0, 0, 0}, {0, 2, 0}, {0, 0, 3}});
+    assertThrows(RuntimeException.class, m1::invert);
+    m1 = new Matrix(new double[][] {{9, -1.001, 3}, {0, 0, 0}, {5, 0, 3}});
+    assertThrows(RuntimeException.class, m1::invert);
+    m1 = new Matrix(new double[][] {{9, 0, 3}, {0, 0, 1.5333}, {2, 0, 1.0 / 7.0}});
+    assertThrows(RuntimeException.class, m1::invert);
+  }
+
+  @Test
+  public void testPlus() {
+    Matrix expected =
+        new Matrix(
+            new double[][] {
+              {1, -2},
+              {0, 0.53},
+            });
+    Matrix m1 =
+        new Matrix(
+            new double[][] {
+              {0, -1},
+              {4, 0.3},
+            });
+    Matrix m2 =
+        new Matrix(
+            new double[][] {
+              {1, -1},
+              {-4, 0.23},
+            });
+    assertEquals(expected, m1.plus(m2));
+    assertEquals(expected, m2.plus(m1));
+  }
+
+  @Test
+  public void testSwitchRows() {
+    Matrix expected =
+        new Matrix(
+            new double[][] {
+              {4, 0.3},
+              {0, -1},
+            });
+    Matrix m1 =
+        new Matrix(
+            new double[][] {
+              {0, -1},
+              {4, 0.3},
+            });
+    assertEquals(expected, m1.switchRows(0, 1));
+    assertThrows(IndexOutOfBoundsException.class, () -> m1.switchRows(1, 2));
   }
 }
