30c5f55b316f91bdb34680f0c3e5d898487dcb8e
Thomas Breydo <tbreydo@gmail.com>
2022-05-17 17:19:21 -0400

    Finish implementing CelestialBody

--- src/com/thomasbreydo/orbitals/CelestialBody.java
+++ src/com/thomasbreydo/orbitals/CelestialBody.java
@@ -5,6 +5,7 @@ import com.thomasbreydo.physics.Vector2D;
 import org.opensourcephysics.display.Circle;
 import org.opensourcephysics.display.Drawable;
 import org.opensourcephysics.display.DrawingPanel;
+import org.opensourcephysics.display.Trail;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -12,15 +13,31 @@ import java.util.ArrayList;
 public class CelestialBody extends FreeBody implements Drawable {
   private static final double G = 6.673889E-11;
   Circle circle;
+  boolean showTrail;
+  Trail trail;
   CelestialSystem system;
   ArrayList<Vector2D> forces;
 
   CelestialBody(Vector2D position, Vector2D velocity, double mass, CelestialSystem system) {
+    this(position, velocity, mass, system, true);
+  }
+
+  CelestialBody(
+      Vector2D position,
+      Vector2D velocity,
+      double mass,
+      CelestialSystem system,
+      boolean showTrail) {
     super(position, velocity, mass);
-    circle = new Circle(this.getX(), this.getY(), 5);
     this.system = system;
+    this.showTrail = showTrail;
+
+    circle = new Circle(this.getX(), this.getY(), 5);
     forces = new ArrayList<>();
     netForce = new Vector2D(0, 0);
+    if (showTrail) {
+      trail = new Trail();
+    }
   }
 
   public Vector2D forceExertedBy(CelestialBody other) {
@@ -43,5 +60,14 @@ public class CelestialBody extends FreeBody implements Drawable {
   public void draw(DrawingPanel drawingPanel, Graphics graphics) {
     circle.setXY(getX(), getY());
     circle.draw(drawingPanel, graphics);
+    if (showTrail) {
+      trail.addPoint(getX(), getY());
+      trail.draw(drawingPanel, graphics);
+    }
+  }
+
+  @Override
+  public void step(double deltaTime) {
+    super.step(deltaTime);
   }
 }
