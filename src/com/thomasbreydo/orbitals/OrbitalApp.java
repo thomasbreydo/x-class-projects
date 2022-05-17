c5531a586cc3d296372e4e29ee2f23885c24c0f7
Thomas Breydo <tbreydo@gmail.com>
2022-05-17 17:21:38 -0400

    Use different Vector2D instances for pos, vel, etc

--- src/com/thomasbreydo/orbitals/OrbitalApp.java
+++ src/com/thomasbreydo/orbitals/OrbitalApp.java
@@ -6,33 +6,45 @@ import org.opensourcephysics.controls.SimulationControl;
 import org.opensourcephysics.frames.PlotFrame;
 
 public class OrbitalApp extends AbstractSimulation {
-  static final double DELTA_TIME = 0.1; // (s)
+  static final double HOURS = 3600; // (s)
+  static final double DELTA_TIME = 12 * HOURS;
   PlotFrame plotFrame;
   CelestialSystem system;
+  CelestialBody earth;
+  CelestialBody sun;
 
   public static void main(String[] args) {
     SimulationControl.createApp(new OrbitalApp());
   }
 
+  public static Vector2D vec(double x, double y) {
+    return new Vector2D(x, y);
+  }
+
   @Override
   public void initialize() {
     super.initialize();
+
+    int fps = 90;
+    setDelayTime(1000 / fps);
+
     double sunMass = control.getDouble("Sun mass (kg)");
-    double earthMass = control.getDouble("Earth mass (kg)");
     double radius = control.getDouble("Distance (m)");
+    double earthMass = control.getDouble("Earth mass (kg)");
+    double earthVx = control.getDouble("Earth Vx (m/s)");
+    double earthVy = control.getDouble("Earth Vy (m/s)");
 
     plotFrame = new PlotFrame("x", "y", "Orbitals");
     plotFrame.setVisible(true);
-    plotFrame.setPreferredMinMax(-1.1 * radius, 1.1 * radius, -1.1 * radius, 1.1 * radius);
+    plotFrame.setPreferredMinMax(-1.5 * radius, 1.5 * radius, -1.5 * radius, 1.5 * radius);
     plotFrame.setDefaultCloseOperation(PlotFrame.EXIT_ON_CLOSE);
     plotFrame.clearDrawables();
 
     system = new CelestialSystem();
     plotFrame.addDrawable(system);
 
-    Vector2D zero = new Vector2D(0, 0);
-    CelestialBody sun = new CelestialBody(zero, zero, sunMass, system);
-    CelestialBody earth = new CelestialBody(new Vector2D(radius, 0), zero, earthMass, system);
+    sun = new CelestialBody(vec(0, 0), vec(0, 0), sunMass, system);
+    earth = new CelestialBody(vec(radius, 0), vec(earthVx, earthVy), earthMass, system);
 
     system.addBody(sun);
     system.addBody(earth);
@@ -42,9 +54,11 @@ public class OrbitalApp extends AbstractSimulation {
   public void reset() {
     super.reset();
 
-    control.setValue("Sun mass (kg)", 2e30);
-    control.setValue("Earth mass (kg)", 6e24);
-    control.setValue("Distance (m)", 1.5e11); // 29831.3 m/s vt
+    control.setValue("Sun mass (kg)", 1.989E30);
+    control.setValue("Earth mass (kg)", 5.972E24);
+    control.setValue("Distance (m)", 1.513E11);
+    control.setValue("Earth Vx (m/s)", 0.0);
+    control.setValue("Earth Vy (m/s)", -29831.3);
   }
 
   @Override
